ffed5f3cf7ac066d4d5698993b1804f6cd285816
Vinay D S <vinay.ds21@gmail.com>
2021-03-08 17:10:47 +0530

    Update array-problem1.js

--- array-problem1.js
+++ array-problem1.js
@@ -1,7 +1,7 @@
 const arr = [2, 3, 1, 5, 4, 7, 19, 10];
 const num = 10;
 
-function arrSum(arr, num) {
+const arrSum = (arr, num) => {
   let outputArr = [];
   for (let i = 0; i < arr.length; i++) {
     for (let j = i+1; j < arr.length; j++) {
