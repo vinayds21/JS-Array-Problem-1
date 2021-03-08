2b03b917d00a90199a55c089d711d1f3855e83bc
Vinay D S <vinay.ds21@gmail.com>
2021-03-08 17:08:52 +0530

    Add files via upload

---
+++ array-problem1.js
@@ -0,0 +1,18 @@
+const arr = [2, 3, 1, 5, 4, 7, 19, 10];
+const num = 10;
+
+function arrSum(arr, num) {
+  let outputArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i+1; j < arr.length; j++) {
+      let sum = arr[i] + arr[j];
+      if (num === sum) {
+        outputArr.push({ first: arr[i], second:arr[j]  });
+      }
+    }
+  }
+  return outputArr;
+}
+
+const invokeFun = arrSum(arr, num);
+console.log("invokeFun", invokeFun);
